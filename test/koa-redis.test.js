ab457f887bca0c7c437469f735bafa2a2b949017
dead_horse <dead_horse@qq.com>
2014-02-01 02:45:30 +0800

    fix expires tll, add test. fix maxage typo

--- test/koa-redis.test.js
+++ test/koa-redis.test.js
@@ -44,6 +44,25 @@ describe('test/lib/koa-redis.test.js', function () {
     })();
   });
 
+  it('should set with cookie.maxage ok', function (done) {
+    co(function *() {
+      yield store.set('key', {a: 1, cookie: {maxage: 1000}});
+      (yield store.get('key')).should.eql({a: 1, cookie: {maxage: 1000}});
+      (yield store.client.ttl(store.prefix + 'key')).should.equal(1);
+      done();
+    })();
+  });
+
+  it('should set with cookie.expires ok', function (done) {
+    co(function *() {
+      var expires = new Date(Date.now() + 1000);
+      yield store.set('key', {a: 1, cookie: {expires: expires}});
+      (yield store.get('key')).should.eql({a: 1, cookie: {expires: expires}});
+      (yield store.client.ttl(store.prefix + 'key')).should.equal(1);
+      done();
+    })();
+  });
+
   it('should expire after 2s', function (done) {
     co(function *() {
       function sleep(t) {
