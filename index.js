98d0b05abd3b0b19d725918f0baa53d4986be66c
Nick Law <nick@nicholaslaw.com.au>
2018-05-26 17:55:56 +1000

    Replace koa-generic-session with koa-session
    Migrating from koa-generic-session to koa-session is a major
    breaking change. This implementation uses Node's util.promisify
    function for converting a callback-based function to a Promise-
    based one, which means Node v8+ is required.
    
    This change also:
    - converts all generators to async/await functions
    - has RedisStore extend EventEmitter, since that is the prefered
      method when using class inheritance
    - dropped backward compatibility for options.socket and options.pass

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-/**!
+/**
  * koa-redis - index.js
- * Copyright(c) 2015
+ * Copyright(c) 2013-2018
  * MIT Licensed
  *
  * Authors:
@@ -9,153 +9,180 @@
 
 'use strict';
 
-/**
- * Module dependencies.
- */
-
-var EventEmitter = require('events').EventEmitter;
-var debug = require('debug')('koa-redis');
-var redis = require('redis');
-var redisWrapper = require('co-redis');
-var util = require('util');
-var wrap = require('co-wrap-all');
+const redis = require('redis');
+const debug = require('debug')('koa-redis');
+const EventEmitter = require('events').EventEmitter;
+const { promisify } = require('util');
 
 /**
- * Initialize redis session middleware with `opts` (see the README for more info):
- *
- * @param {Object} options
- *   - {Object} client       redis client (overides all other options except db and duplicate)
- *   - {String} socket       redis connect socket (DEPRECATED: use 'path' instead)
- *   - {String} db           redis db
- *   - {Boolean} duplicate   if own client object, will use node redis's duplicate function and pass other options
- *   - {String} pass         redis password (DEPRECATED: use 'auth_pass' instead)
- *   - {Any} [any]           all other options inclduing above are passed to node_redis
+ * @extends EventEmitter
  */
-var RedisStore = module.exports = function (options) {
-  if (!(this instanceof RedisStore)) {
-    return new RedisStore(options);
-  }
-  EventEmitter.call(this);
-  options = options || {};
+module.exports = class RedisStore extends EventEmitter {
+  /**
+   * @param {Object} options The options for creating a new RedisStore
+   * @param {Object} options.client Redis client (overides all other options except db and duplicate)
+   * @param {String} options.db The Redis database to use
+   * @param {Boolean} options.duplicate If own client object, will use node Redis' duplicate function and pass other options
+   * @param {String} options.password Redis password (use of 'auth_pass' is also permitted)
+   * @param {String} options.path The UNIX socket string of the Redis server
+   * @param {Any} options.[any] All other options including above are passed to node_redis
+   */
+  constructor(options) {
+    super();
 
-  var client;
-  options.auth_pass = options.auth_pass || options.pass || null;     // For backwards compatibility
-  options.path = options.path || options.socket || null;             // For backwards compatibility
-  if (!options.client) {
-    debug('Init redis new client');
-    client = redis.createClient(options);
-  } else {
-    if (options.duplicate) {                                         // Duplicate client and update with options provided
-      debug('Duplicating provided client with new options (if provided)');
-      var dupClient = options.client;
-      delete options.client;
-      delete options.duplicate;
-      client = dupClient.duplicate(options);                         // Useful if you want to use the DB option without adjusting the client DB outside koa-redis
+    if (!this instanceof RedisStore) {
+      return new RedisStore(options);
+    }
+
+    options = options || {};
+    options.path = options.path || null;
+    options.password = options.password || options.auth_pass || null; // For backwards compatibility
+
+    let client;
+
+    if (!options.client) {
+      debug('Initializing redis new client');
+      client = redis.createClient(options);
     } else {
-      debug('Using provided client');
-      client = options.client;
+      if (options.duplicate) { // Duplicate client and update with options provided
+        debug('Duplicating provided client with new options (if provided)');
+        const dupClient = options.client;
+        delete options.client;
+        delete options.duplicate;
+        client = dupClient.duplicate(options); // Useful if you want to use the DB option without adjusting the client DB outside koa-redis
+      } else {
+        debug('Using provided client');
+        client = options.client;
+      }
+    }
+
+    if (options.db) {
+      debug('selecting db %s', options.db);
+      client.select(options.db);
+      client.on('connect', () => {
+        client.send_anyways = true;
+        client.select(options.db);
+        client.send_anyways = false;
+      });
+    }
+
+    client.on('error', this.emit.bind(this, 'error'));
+    client.on('end', this.emit.bind(this, 'end'));
+    client.on('end', this.emit.bind(this, 'disconnect')); // For backwards compatibility
+    client.on('connect', this.emit.bind(this, 'connect'));
+    client.on('reconnecting', this.emit.bind(this, 'reconnecting'));
+    client.on('ready', this.emit.bind(this, 'ready'));
+    client.on('warning', this.emit.bind(this, 'warning'));
+
+    this.on('connect', () => {
+      debug('connected to redis');
+      this.connected = client.connected;
+    });
+
+    this.on('ready', () => {
+      debug('redis ready');
+    });
+
+    this.on('end', () => {
+      debug('redis ended');
+      this.connected = client.connected;
+    });
+
+    this.on('error', () => {
+      debug('redis error');
+      this.connected = client.connected;
+    });
+
+    this.on('reconnecting', () => {
+      debug('redis reconnecting');
+      this.connected = client.connected;
+    });
+
+    this.on('warning', () => {
+      debug('redis warning');
+      this.connected = client.connected;
+    });
+
+    this._redisClient = client;
+
+    // Promisify all the client methods we care about (ie. the ones we use)
+    [ 'get', 'set', 'setex', 'del' ].forEach(method => {
+      client[method] = promisify(client[method]).bind(client);
+    });
+
+    this.client = client;
+    this.connected = client.connected;
+
+    // Support optional serialize and unserialize
+    this.serialize = (typeof options.serialize === 'function' && options.serialize) || JSON.stringify;
+    this.unserialize = (typeof options.unserialize === 'function' && options.unserialize) || JSON.parse;
+  }
+
+  /**
+   * @param {String} sid The session key to retrieve
+   * @return {(String | null)} The session value retrieved
+   */
+  async get(sid) {
+    const value = await this.client.get(sid);
+    debug('get session: %s', value || 'none');
+
+    if (!value) {
+      return null;
+    }
+
+    try {
+      return this.unserialize(value.toString());
+    } catch (err) {
+      // ignore error
+      debug('parse session error: %s', err.message);
     }
   }
 
-  if (options.db) {
-    debug('selecting db %s', options.db)
-    client.select(options.db);
-    client.on('connect', function() {
-      client.send_anyways = true;
-      client.select(options.db);
-      client.send_anyways = false;
-    });
+  /**
+   * @param {String} sid The session key to set
+   * @param {Object} sess The session value to store
+   * @param {Number} ttl The max-age (in ms) the key will live for before expiring
+   * @return {void}
+   */
+  async set(sid, sess, ttl) {
+    if (typeof ttl === 'number') {
+      ttl = Math.ceil(ttl / 1000);
+    }
+
+    sess = this.serialize(sess);
+
+    if (ttl) {
+      debug('SETEX %s %s %s', sid, ttl, sess);
+      await this.client.setex(sid, ttl, sess);
+    } else {
+      debug('SET %s %s', sid, sess);
+      await this.client.set(sid, sess);
+    }
+    debug('SET %s complete', sid);
   }
 
-  client.on('error', this.emit.bind(this, 'error'));
-  client.on('end', this.emit.bind(this, 'end'));
-  client.on('end', this.emit.bind(this, 'disconnect'));              // For backwards compatibility
-  client.on('connect', this.emit.bind(this, 'connect'));
-  client.on('reconnecting', this.emit.bind(this, 'reconnecting'));
-  client.on('ready', this.emit.bind(this, 'ready'));
-  client.on('warning', this.emit.bind(this, 'warning'));
-  this.on('connect', function() {
-    debug('connected to redis');
-    this.connected = client.connected;
-  });
-  this.on('ready', function() {
-    debug('redis ready');
-  });
-  this.on('end', function() {
-    debug('redis ended');
-    this.connected = client.connected;
-  });
-  // No good way to test error
-  /* istanbul ignore next */
-  this.on('error', function() {
-    debug('redis error');
-    this.connected = client.connected;
-  });
-  // No good way to test reconnect
-  /* istanbul ignore next */
-  this.on('reconnecting', function() {
-    debug('redis reconnecting');
-    this.connected = client.connected;
-  });
-  // No good way to test warning
-  /* istanbul ignore next */
-  this.on('warning', function() {
-    debug('redis warning');
-    this.connected = client.connected;
-  });
-
-  //wrap redis
-  this._redisClient = client;
-  this.client = redisWrapper(client);
-  this.connected = client.connected;
-
-  // support optional serialize and unserialize
-  this.serialize = (typeof options.serialize === 'function' && options.serialize) || JSON.stringify;
-  this.unserialize = (typeof options.unserialize === 'function' && options.unserialize) || JSON.parse;
-};
-
-util.inherits(RedisStore, EventEmitter);
-
-RedisStore.prototype.get = function *(sid) {
-  var data = yield this.client.get(sid);
-  debug('get session: %s', data || 'none');
-  if (!data) {
-    return null;
+  /**
+   * @param {String} sid The session key to destroy
+   * @return {void}
+   */
+  async destroy(sid) {
+    debug('DEL %s', sid);
+    await this.client.del(sid);
+    debug('DEL %s complete', sid);
   }
-  try {
-    return this.unserialize(data.toString());
-  } catch (err) {
-    // ignore err
-    debug('parse session error: %s', err.message);
+
+  /**
+   * @return {void}
+   */
+  async quit() { // End connection SAFELY
+    debug('quitting redis client');
+    await this.client.quit();
+  }
+
+  /**
+   * @return {void}
+   */
+  async end() {
+    await this.quit();
   }
 };
-
-RedisStore.prototype.set = function *(sid, sess, ttl) {
-  if (typeof ttl === 'number') {
-    ttl = Math.ceil(ttl / 1000);
-  }
-  sess = this.serialize(sess);
-  if (ttl) {
-    debug('SETEX %s %s %s', sid, ttl, sess);
-    yield this.client.setex(sid, ttl, sess);
-  } else {
-    debug('SET %s %s', sid, sess);
-    yield this.client.set(sid, sess);
-  }
-  debug('SET %s complete', sid);
-};
-
-RedisStore.prototype.destroy = function *(sid) {
-  debug('DEL %s', sid);
-  yield this.client.del(sid);
-  debug('DEL %s complete', sid);
-};
-
-RedisStore.prototype.quit = function* () {                         // End connection SAFELY
-  debug('quitting redis client');
-  yield this.client.quit();
-};
-
-wrap(RedisStore.prototype);
-
-RedisStore.prototype.end = RedisStore.prototype.quit;              // End connection SAFELY. The real end() command should never be used, as it cuts off to queue.
